963f879a406d688fc871e215a24cf495da8046f5
abdul <abdul.tanveer@gmail.com>
2024-08-16 15:02:16 +0530

    filterable products table

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import "./App.css";
 import "./styles.css";
 import Game from "./components/Game";
+import { FilterableProductTable } from "./components/products/FilterableProductTable";
+const PRODUCTS = [
+  { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+  { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+  { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" },
+];
 
 function App() {
   return (
     <div className="App">
-      <Game />
+      <FilterableProductTable products={PRODUCTS} />;
     </div>
   );
 }
